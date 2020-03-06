02bc1a4bccfab6c158cdaff70a0a5a17e32fbd88
Beat Ottiger <ottigerb@gmail.com>
2020-03-06 13:59:13 +0100

    Update rs13_score.js

--- calculations/rs13_score.js
+++ calculations/rs13_score.js
@@ -5,119 +5,155 @@ function main(responses) {
     // H e l p e r   -   F U N C T I O N S
     // ------------------------------------------
 
-    calc.roundToTwo = function(num) {
+    calc.roundToTwo = function (num) {
         // Round a Number to 0.X 
         return +(Math.round(num + "e+2") + "e-2");
     };
 
+    // CH Datumsformat
+    calc.formatDateCH = function (date_string) {
+        date_string = date_string || null
+        if (date_string !== null) {
+
+            // 1952-11-19T00:00:00.000000000000Z
+            var year = parseInt(date_string.substring(0, 4));
+            var month = parseInt(date_string.substring(5, 7));
+            var day = parseInt(date_string.substring(8, 10));
+            var date_string_return = day + "." + month + "." + year
+
+            return date_string_return;
+        } else {
+            return null;
+        }
+    };
+
+    // ------------------------------------------
+    // F U N C T I O N S
+    // ------------------------------------------
+
+    calc.getInterpretation = function (d, resp, score, range) {
+
+        var return_text = "";
+
+        console.log('ddddd', resp);
+
+        if (d.patient.data.gender === "male") {
+            return_text = "Herr " + d.patient.data.last_name;
+        } else {
+            return_text = "Frau " + d.patient.data.last_name;
+        };
+
+        return_text = return_text + " erreichte beim Resilienzfragebogen (RS-13) vom";
+
+        return_text = return_text + " " + calc.formatDateCH(resp.data.filled) + " ";
+        return_text = return_text + "einen Summenscore von " + score + ", ";
+        return_text = return_text + "was auf eine " + range.interpretation_de + " hindeutet.";
+
+        return return_text;
+    };
+    
 
     // ------------------------------------------
     // F U N C T I O N  -  Main
     // ------------------------------------------
-    calc.getResults = function(d) {
-
-        var return_obj = {};
+    calc.getResults = function (d) {
 
         var responses_array = d.survey_responses;
         var allResults = [];
 
-        responses_array.forEach(function(response, myindex) {
+        responses_array.forEach(function (response, myindex) {
 
 
-            var myResults = {};
+            var myResults = {
+                "rs13": {},
+                "rs13_score": 0,
+                "interpretation": "",
+                "range": null,
+                "ranges": null
+            };
             var result = response.data.response;
 
+            // Save Integer
+            myResults.rs13.rs13_01 = parseInt(result.rs13_01);
+            myResults.rs13.rs13_02 = parseInt(result.rs13_02);
+            myResults.rs13.rs13_03 = parseInt(result.rs13_03);
+            myResults.rs13.rs13_04 = parseInt(result.rs13_04);
+            myResults.rs13.rs13_05 = parseInt(result.rs13_05);
+            myResults.rs13.rs13_06 = parseInt(result.rs13_06);
+            myResults.rs13.rs13_06 = parseInt(result.rs13_06);
+            myResults.rs13.rs13_07 = parseInt(result.rs13_07);
+            myResults.rs13.rs13_08 = parseInt(result.rs13_08);
+            myResults.rs13.rs13_09 = parseInt(result.rs13_09);
+            myResults.rs13.rs13_10 = parseInt(result.rs13_10);
+            myResults.rs13.rs13_11 = parseInt(result.rs13_11);
+            myResults.rs13.rs13_12 = parseInt(result.rs13_12);
+            myResults.rs13.rs13_13 = parseInt(result.rs13_13);
 
-            if (result.hasOwnProperty("TMTATime")) {
-                result.tmt_a_error = result.TMTAError;
-                result.tmt_a_time = result.TMTATime;
-                result.tmt_b_error = result.TMTBError;
-                result.tmt_b_time = result.TMTBTime;
-                result.edu_years = result.Ausbildungsjahre;
-                result.mz = result.Messzeitpunkt;
+            // SumScore
+            myResults.rs13_score = myResults.rs13.rs13_01 +
+                myResults.rs13.rs13_02 +
+                myResults.rs13.rs13_03 +
+                myResults.rs13.rs13_04 +
+                myResults.rs13.rs13_05 +
+                myResults.rs13.rs13_06 +
+                myResults.rs13.rs13_07 +
+                myResults.rs13.rs13_08 +
+                myResults.rs13.rs13_09 +
+                myResults.rs13.rs13_10 +
+                myResults.rs13.rs13_11 +
+                myResults.rs13.rs13_12 +
+                myResults.rs13.rs13_13;
+
+
+
+            var current_range = {};
+
+            var ranges = [{
+                "from": 13,
+                "to": 66,
+                "interpretation_de": "niedrige Widerstandskraft (Resilienz)",
+                "result_color": "#F44336"
+            }, {
+                "from": 67,
+                "to": 72,
+                "interpretation_de": "moderate Widerstandskraft (Resilienz)",
+                "result_color": "#FF9800"
+            }, {
+                "from": 73,
+                "to": 91,
+                "interpretation_de": "hohe Widerstandskraft (Resilienz)",
+                "result_color": "#4CAF50"
+            }];
+
+            current_range = ranges[0];
+
+            if (myResults.rs13_score >= ranges[1].from) {
+                current_range = ranges[1];
+            };
+            if (myResults.rs13_score >= ranges[2].from) {
+                current_range = ranges[2];
             };
 
-
-            // Age & Edu
-            myResults.birthdate = d.patient.data.birthdate
-
-            var set_age = calc.getPatientAgeMz(d.patient.data.birthdate, response.data.filled);
-            myResults.set_age = set_age;
-
-            var edu_years = calc.roundToTwo(result.edu_years);
-            myResults.edu_years = edu_years;
+            myResults.range = current_range;
+            myResults.ranges = ranges;
 
 
-            // Erhebungszeitpunkt
-            myResults.date = response.data.filled;
-            
-            if (result.hasOwnProperty("Erhebungsdatum")) {
-                myResults.date = result.Erhebungsdatum;
-            };
-            
+            myResults.interpretation = calc.getInterpretation(d, response, myResults.rs13_score, myResults.range);
 
-
-            // Zeit & Fehler in Integer
-            myResults.TMTAError = parseInt(result.tmt_a_error);
-            myResults.TMTBError = parseInt(result.tmt_b_error);
-            myResults.TMTATime = parseInt(result.tmt_a_time);
-            myResults.TMTBTime = parseInt(result.tmt_b_time);
-
-
-            // Calculate Stuff
-            myResults.quotient = calc.quotient(result);
-            myResults.quotient_rounded = calc.roundToTwo(calc.quotient(result));
-            myResults.percentile = calc.get_percentile(result, set_age, edu_years);
-
-            // Messzeitpunkt
-            var Messzeitpunkt = parseInt(result.mz);
-            myResults.mz = Messzeitpunkt;
-
-            var Messzeitpunkt_Text = 'Undefined';
-            var Messzeitpunkt_Text_Quotient = 'Undefined';
-
-            if (Messzeitpunkt === 1) {
-                Messzeitpunkt_Text = 'Eintritt';
-                Messzeitpunkt_Text_Quotient = Messzeitpunkt_Text + ' (B/A: ' + myResults.quotient_rounded + ')';
-            };
-            if (Messzeitpunkt === 2) {
-                Messzeitpunkt_Text = 'Austritt';
-                Messzeitpunkt_Text_Quotient = Messzeitpunkt_Text;
-            };
-            if (Messzeitpunkt === 3) {
-                Messzeitpunkt_Text = 'Anderer Messzeitpunkt';
-                Messzeitpunkt_Text_Quotient = Messzeitpunkt_Text;
-            };
-
-            var mz_obj = {
-                "Messzeitpunkt": Messzeitpunkt,
-                "Messzeitpunkt_Text": Messzeitpunkt_Text,
-                "Messzeitpunkt_Text_Quotient": Messzeitpunkt_Text_Quotient
-            };
-
-            myResults.Messzeitpunkt = mz_obj;
 
             // Write Results for the Return
             // Do not modify stuff here
             myResults.hash = result['optinomixHASH'];
             myResults.response = response;
-            // myResults.d = d;
-
-            myResults.version = "19. Oktober 2016";
-
             allResults.push(myResults);
 
         });
-
-        return_obj.responses_array = responses_array;
-        return_obj.allResults = allResults;
-        return_obj.full = d;
 
         return allResults;
     };
 
 
     // Return
-    // return calc.getResults(responses);
-    return responses;
+    return calc.getResults(responses);
+    // return responses;
 }
