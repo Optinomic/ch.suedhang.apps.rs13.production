435c7da38dfe6b71b71b5f204a94f6d6d6083c9d
Beat Ottiger <ottigerb@gmail.com>
2020-03-13 16:37:31 +0100

    PDF

--- dist/javascript/vue_component_app_rs13-original.js
+++ dist/javascript/vue_component_app_rs13-original.js
@@ -2,6 +2,11 @@
 Vue.component('app-rs13', {
     props: {},
     created() {},
+    data: function () {
+        return {
+            pdf_content: []
+        }
+    },
     computed: {
         sr_data() {
             // return data
@@ -9,6 +14,44 @@ Vue.component('app-rs13', {
                 return this.$store.state.sr.data;
             } catch (e) {
                 return [];
+            };
+        },
+        pdf_ready() {
+            // return data
+            try {
+                if (this.$store.state.sr.data.length) {
+
+                    var pdf = [];
+
+                    pdf.push(makepdf._suedhang_logo_anschrift());
+
+                    var title = makepdf._title("Psychische Wiederstandskraft", "Resilienzfragebogen (RS-13)");
+                    pdf.push(title);
+
+                    pdf.push(makepdf._heading("Interpretation", null, "h2"));
+
+                    if (this.$store.state.sr.data.length > 0) {
+
+                        this.$store.state.sr.data.forEach(function (d) {
+                            if (d.calculation_found) {
+                                var interpret = makepdf._text(d.calculation.resilienz_score.interpretation);
+                                pdf.push(makepdf._keepTogether(interpret));
+                            };
+                        });
+
+                    } else {
+                        pdf.push(makepdf._noData("Resilienz", 6));
+                    };
+
+
+                    this.pdf_content = pdf;
+                    return true;
+                } else {
+                    return false;
+                };
+
+            } catch (e) {
+                return false;
             };
         }
     },
@@ -19,6 +62,7 @@ Vue.component('app-rs13', {
                 <v-divider></v-divider>
             </div>
             <div style="margin-bottom:24px;">
+            
                 <v-card class="mx-auto" outlined v-for="sr in sr_data" :key="sr.event_id">
                     <v-card-text v-if="sr.calculation_found">
                         <div>
@@ -40,6 +84,11 @@ Vue.component('app-rs13', {
                         </div>
                     </v-card-text>
                 </v-card>
+            </div>
+            <div style="margin-bottom:24px;" v-if="pdf_ready">
+                <h3 class="font-weight-light">Druckvorlage (PDF)</h3>
+                <v-divider></v-divider>
+                <optinomic-pdfmake header-left="Herr Muster" footer-left="Resilienz" header-right="Klinik Südhang" document-title="Resilienz" :content="pdf_content" hide-logo></optinomic-pdfmake>
             </div>
         </div>
     `
