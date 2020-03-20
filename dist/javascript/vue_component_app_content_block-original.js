fdd68fa272c33b83749380a9957e38b86245b958
Beat Ottiger <ottigerb@gmail.com>
2020-03-20 15:27:41 +0100

    Color

--- dist/javascript/vue_component_app_content_block-original.js
+++ dist/javascript/vue_component_app_content_block-original.js
@@ -20,8 +20,8 @@ Vue.component('optinomic-content-block', {
     },
     template: `
         <div class="mt-10 mb-10" :id="id">
-            <p class="overline" style="margin-left:1px" v-html="subtitle"></p>
-            <h2 class="headline font-weight-light text--secondary" v-html="title" style="margin-top:-22px;color:#8b0042"></h2>
+            <p class="overline" style="margin-left:1px;color:#8b0042" v-html="subtitle"></p>
+            <h2 class="headline font-weight-light text--secondary" v-html="title" style="margin-top:-22px"></h2>
             <v-divider></v-divider>
             <div class="mt-4 mb-4">
                 <slot></slot>
